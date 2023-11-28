fd73ecee8ea1428d27196851cde7c97b17a34db4
unknown <fathinr16@gmail.com>
2023-11-28 08:25:47 +0700

    first commit

---
+++ lib/screens/station_details.dart
@@ -0,0 +1,72 @@
+// station_detail.dart
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../models/station.dart';
+
+class StationDetailPage extends StatelessWidget {
+  final Station station;
+
+  const StationDetailPage({Key? key, required this.station}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Station Detail'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              Image.asset(
+                'assets/images/kai.png',
+                height: 80,
+              ),
+              const SizedBox(height: 25),
+              Text(
+                'Kode Stasiun: ${station.code}',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'STASIUN ${station.name}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'KOTA ${station.city}',
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '${station.cityname}',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          _launchURL('https://www.kai.id/' ?? '');
+        },
+        icon: Icon(Icons.open_in_browser),
+        label: Text("See More"),
+      ),
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
